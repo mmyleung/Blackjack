79d1bc72f69af2ad63404756adee6d270c419e81
mmyleung <minamleung@gmail.com>
2023-05-08 14:24:06 +0100

    condense code

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-
+var playerNumber = 0;
+var dealerNumber = 0;
+confirmStart();
 
 //function that returns number inclusive of min and max
 function getRndInteger(min, max) {
@@ -8,19 +10,24 @@ function getRndInteger(min, max) {
 
 // Psuedocode
 // Confirm to start the game play blackjack
+function confirmStart() {
+if (confirm(`Want to play Blackjack?`)) {
+    playerNumber = 0;
+    dealerNumber = 0;
+    startGame();
+} else {
+    alert(`Refresh the page to restart.`)
+}
+}
 // Player gets a random number between 4-21 (replicates 2 cards)
-var playerNumber = 0;
-var dealerNumber = 0;
-
-function assignInitialPNo() {
-    playerNumber = playerNumber + getRndInteger(4, 21);
-}
-
-
 // Dealer gets a random number between 2-11 (replicates 1 card)
-function assignInitialDNo() {
-    dealerNumber = dealerNumber + getRndInteger(2,11)
+
+function assignInitialCards() {
+    playerNumber = playerNumber + getRndInteger(4, 21);
+    dealerNumber = dealerNumber + getRndInteger(2,11);
+    return;
 }
+
 
 // User will see own number and dealer number
 //If number is equal to 21 then player wins
@@ -30,27 +37,30 @@ function blackJack (number) {
     console.log(number);
     if (number === 21) {
         alert(`You have ${playerNumber} - YOU WIN! :)`)
+        confirmStart();
         return;
     } else if (number > 21) {
         alert(`You have ${playerNumber} - YOU LOST! :(`)
+        confirmStart();
         return;
     } else {
         return;
     }
 }
 
-assignInitialDNo();
-assignInitialPNo();
-blackJack(playerNumber);
+function startGame() {
+    assignInitialCards();
+    blackJack(playerNumber);
+
 
 // Player can decide to hit or stay
-let playerDecision = () => confirm(`You have been dealt ${playerNumber}, the dealer has been dealt ${dealerNumber}. Press ok to hit or cancel to stay`);
+playerDecision = () => confirm(`You have been dealt ${playerNumber}, the dealer has been dealt ${dealerNumber}. Press ok to hit or cancel to stay`);
 
-if (playerDecision() == true) {
+while (playerDecision() == true) {
     //Check that 
     hit();
     blackJack(playerNumber);
-} else {
+} if (playerDecision() == false) {
     stay();
 }
 // If hit add a random number between 2-11 to existing number
@@ -59,6 +69,7 @@ if (playerDecision() == true) {
 
 function hit() {
         playerNumber = playerNumber + getRndInteger(2, 11);
+        console.log(playerNumber);
         };
 
 // if {
@@ -73,23 +84,36 @@ function hit() {
 function stay() {
 
     while (dealerNumber < 17) {
-        assignInitialDNo();
-        alert(`The dealer has ${dealerNumber}`);
+        addDealerNo();
+        alert(`You are staying at ${playerNumber}, the dealer has ${dealerNumber}`);
     }
     if (dealerNumber > 21) {
-        alert(`You have ${playerNumber} and the dealer has ${dealerNumber}. YOU WIN! :)`);
+        alert(`Dealer has busted at ${dealerNumber}. YOU WIN! :)`);
     } else {
         if (playerNumber > dealerNumber) {
-            alert(`You have ${playerNumber} and the dealer has ${dealerNumber}. YOU WIN! :)`)
-        } else if (playerDecision < dealerNumber) {
-            alert(`You have ${playerNumber} and the dealer has ${dealerNumber}. YOU LOSE! :(`)
+            alert(`You have ${playerNumber} and the dealer has ${dealerNumber}. YOU WIN! :)`);
+            confirmStart();
+        } else if (playerNumber < dealerNumber) {
+            alert(`You have ${playerNumber} and the dealer has ${dealerNumber}. YOU LOSE! :(`);
+            confirmStart();
         } else {
-            alert(`You have ${playerNumber} and the dealer has ${dealerNumber}. IT'S A TIE!`)
+            alert(`You have ${playerNumber} and the dealer has ${dealerNumber}. IT'S A TIE!`);
+            confirmStart();
         }
     }
 }
 // If dealer number is less than 17, then add random number between 2-11 to existing number
+
+//function to add to dealer number
+
+function addDealerNo () {
+    dealerNumber = dealerNumber + getRndInteger(2,11);
+}
 // Evaluate again and repeat if dealer number is less than 17
 // If dealer number is more than 17 then stop
 // If player or dealer number is more than 21 then they lose
 // If both stop and under 21 then compare and see which has the closer number to 21
+
+
+}
+
